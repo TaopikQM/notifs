a82f9ddadcb9f14e41df9a0308e0fd6bf50adfb4
TaopikQM <taopik61318@gmail.com>
2026-09-13 10:06:05 +0700

    Remove icon from FCM notification payload
    Removed the icon from the notification payload for FCM.

--- pages/api/send-notification.js
+++ pages/api/send-notification.js
@@ -117,7 +117,7 @@ export default async function handler(req, res) {
       allTokens.map(token =>
         messaging.send({
           token,
-          notification: { title, body, icon: '/dolan.png' },
+          notification: { title, body },
           webpush: {
             notification: { requireInteraction: true, icon: '/dolan.png' },
             fcmOptions: { link: 'https://ns.vercel.app/user' }
