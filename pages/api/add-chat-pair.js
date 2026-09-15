536654fb29c292d48cafef133d9553bba308b3ab
TaopikQM <taopik61318@gmail.com>
2026-09-15 20:21:38 +0700

    Update add-chat-pair.js

--- pages/api/add-chat-pair.js
+++ pages/api/add-chat-pair.js
@@ -1,4 +1,4 @@
-import { ref, get, push, update, database} from "../../lib/firebase";
+import { ref, get, push, update, set, database} from "../../lib/firebase";
 import { getWIBTime } from "../../lib/firebase";
 
 export default async function handler(req, res) {
