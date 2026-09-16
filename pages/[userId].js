b1a477e1daef497c1ffd9da56bab1e1dc2d4a758
TaopikQM <taopik61318@gmail.com>
2026-09-16 21:43:49 +0700

    Refactor online status indicator for user presence

--- pages/[userId].js
+++ pages/[userId].js
@@ -1049,20 +1049,32 @@ if (!otherUser) {
               {userId.charAt(0).toUpperCase()}
             </div>
              <div className="absolute -bottom-1 -right-1">
-                {currentUserPresence?.status === "online" && (
+                {/* {currentUserPresence?.status === "online" && (
                   <span className="relative flex h-3 w-3">
                     <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"></span>
                     <span className="relative inline-flex rounded-full h-3 w-3 bg-emerald-500"></span>
                   </span>
-                )}
+                )}*/}
+{currentUserPresence?.status === "online" ? (
+  <span className="relative flex h-3 w-3">
+    <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-emerald-400 opacity-75"></span>
+    <span className="relative inline-flex rounded-full h-3 w-3 bg-emerald-500"></span>
+  </span>
+) : (
+  <span className="relative flex h-3 w-3">
+   <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-75"></span>
+                  
+    <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
+  </span>
+)}
                 
-                {/* Tambahan kondisi untuk OFFLINE */}
+                {/* Tambahan kondisi untuk OFFLINE  <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-75"></span>
+                   
                 {currentUserPresence?.status !== "online" && (
                   <span className="relative flex h-3 w-3">
-                   <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-500 opacity-75"></span>
                     <span className="relative inline-flex rounded-full h-3 w-3 bg-red-500"></span>
                   </span>
-                )}
+                )}*/}
               </div>
                {/* Online indicator di avatar */}
 {/*{otherUserStatus?.status === "online" && (
